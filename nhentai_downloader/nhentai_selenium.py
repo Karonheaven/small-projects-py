61c5957e03a21bc0775d34ae77e54b09faceb676
Karonheaven <karonheaven@gmail.com>
2021-05-08 17:19:23 +0800

    LAPTOP-Update NH

--- nhentai_downloader/nhentai_selenium.py
+++ nhentai_downloader/nhentai_selenium.py
@@ -1,22 +1,25 @@
 #!/usr/bin/env python
 # coding:utf-8
 """
-# 
+# 使用Selenium
 # Author: Karonheaven
 """
 # +---------------+需要的包+---------------+
 # 标准库导入
 from typing import *
 
-
 # 第三方库导入
-
+from selenium import webdriver
 
 # 本地库/自定义库导入
 
 
 # +---------------+全局变量&预定义+---------------+
+driver = webdriver.Chrome()
 
+# 添加参数
+opts = webdriver.ChromeOptions()
+opts.add_argument(argument="--disable-gpu")
 
 # +---------------+主程序+---------------+
 pass
